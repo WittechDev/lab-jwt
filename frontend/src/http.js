ffe41e0d5a9e6cd420913e407a2c02c6c0907cb2
crazydogm <crazydogm486@gmail.com>
2025-03-05 21:11:20 +0700

    ✨ Add custom axios & refresh token handler.

---
+++ frontend/src/http.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+const API_HOST = import.meta.env.VITE_API_HOST;
+
+const axiosInstance = axios.create({
+  baseURL: `${API_HOST}/api`,
+  timeout: 1000,
+});
+
+axiosInstance.interceptors.request.use(
+  (request) => {
+    const accessToken = sessionStorage.getItem("accessToken");
+    if (accessToken) {
+      request.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response, // Directly return successful responses.
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true; // Mark the request as retried to avoid infinite loops.
+      try {
+        const refreshToken = sessionStorage.getItem("refreshToken"); // Retrieve the stored refresh token.
+        // Make a request to your auth server to refresh the token.
+        const response = await axios.post(
+          `${API_HOST}/api/auth/refresh`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          }
+        );
+        const { access_token, refresh_token: newRefreshToken } = response.data;
+        // Store the new access and refresh tokens.
+        sessionStorage.setItem("accessToken", access_token);
+        sessionStorage.setItem("refreshToken", newRefreshToken);
+        // Update the authorization header with the new access token.
+        axiosInstance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${access_token}`;
+        return axiosInstance(originalRequest); // Retry the original request with the new access token.
+      } catch (refreshError) {
+        // Handle refresh token errors by clearing stored tokens and redirecting to the login page.
+        console.error("Token refresh failed:", refreshError);
+        sessionStorage.removeItem("accessToken");
+        sessionStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error); // For all other errors, return the error as is.
+  }
+);
+
+export default axiosInstance;
